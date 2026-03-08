8f2524d73f5e6bea6136b4b79e78bdde3455e2a4
Jhony279 <lordjhony732@gmail.comm>
2026-03-08 03:41:53 -0700

    Added comments and fixed the avereage value output (Finished assignment)

--- main.cpp
+++ main.cpp
@@ -4,19 +4,18 @@
 using namespace std;
 
 struct Node {
-    int rating;
+    double rating;
     string comment;
     Node* next;
 };
 
-void insertNodeFront(Node *&, int, string);
-void insertNodeEnd(Node *&, int, string);
+void insertNodeFront(Node *&, double, string);
+void insertNodeEnd(Node *&, double, string);
 void displayRatings(Node *);
 
 
 /**
- * @brief - Description of main
- * @return 
+ * @brief Enter review rating and commoent into linked list
 */
 int main() {
     bool exit = false;
@@ -26,6 +25,7 @@ int main() {
         int choice;
         string leave;
 
+        // ask the user to choose how to insert new nodes until they enter a valid choice
         if (!choice == 1 || !choice == 2) {
             cout << "Please enter 1 or 2." << endl;
             cout << "   [1] New nodes are added at the head of the linked list" << endl;
@@ -33,7 +33,7 @@ int main() {
             cout << "   Choice: "; cin >> choice;
         }
 
-        if (choice == 1) {
+        if (choice == 1) {  // insert new nodes at the head of the linked list
             cout << "Enter review rating 0-5: "; int rating; cin >> rating;
             cout << "Enter review comments: "; string comment; 
             cin.ignore();
@@ -41,7 +41,7 @@ int main() {
 
             insertNodeFront(head, rating, comment);
             cout << "Enter another review? Y/N: "; cin >> leave;
-        } else if (choice == 2) {
+        } else if (choice == 2) {   // insert new nodes at the tail of the linked list
             cout << "Enter review rating 0-5: "; int rating; cin >> rating;
             cout << "Enter review comments: "; string comment; 
             cin.ignore();
@@ -49,11 +49,12 @@ int main() {
 
             insertNodeEnd(head, rating, comment);
             cout << "Enter another review? Y/N: "; cin >> leave;
-        } else {
+        } else {    // Show error message and ask for input again
             cout << "Invalid choice. ";
             continue; // skip the rest of the loop and ask for input again
         }
 
+        // display all reviews and exit the program
         if (leave == "N" || leave == "n") {
             exit = true;
             displayRatings(head);
@@ -63,7 +64,13 @@ int main() {
     return 0;
 }
 
-void insertNodeEnd(Node *&hd, int rating, string comment){
+/**
+ * @brief insert a new node at the end of the linked list
+ * @param hd pointer to the head of the linked list
+ * @param rating the rating value to be stored in the new node
+ * @param comment the comment string to be stored in the new node
+*/
+void insertNodeEnd(Node *&hd, double rating, string comment){
     // insert a node at the end of the list
     Node * newnode = new Node;
     newnode->rating = rating;
@@ -83,7 +90,13 @@ void insertNodeEnd(Node *&hd, int rating, string comment){
     }
 }
 
-void insertNodeFront(Node *&hd, int rating, string comment){
+/**
+ * @brief insert a new node at the front of the linked list
+ * @param hd pointer to the head of the linked list
+ * @param rating the rating value to be stored in the new node
+ * @param comment the comment string to be stored in the new node
+*/
+void insertNodeFront(Node *&hd, double rating, string comment){
     // insert a node at the front of the list
     Node * newnode = new Node;
     newnode->rating = rating;
@@ -94,6 +107,10 @@ void insertNodeFront(Node *&hd, int rating, string comment){
     hd = newnode;
 }
 
+/**
+ * @brief Output the linked list of reviews and calculate the average rating
+ * @param hd pointer to the head of the linked list
+*/
 void displayRatings(Node * hd) {
     // output the linked list
     if (!hd) {
@@ -101,7 +118,7 @@ void displayRatings(Node * hd) {
         return;
     }
     int count = 1;
-    int totalRating = 0;
+    double totalRating = 0;
     Node * current = hd;
     // traverse the list and output each node's value
     cout << "Outputting all reviews:\n";
@@ -111,5 +128,5 @@ void displayRatings(Node * hd) {
         totalRating += current->rating;
         current = current->next;
     }
-    cout << "   >Average: " << totalRating/(count) << endl;
+    cout << "   > Average: " << totalRating/(count - 1) << endl;
 }
